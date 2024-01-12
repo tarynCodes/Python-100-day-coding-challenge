77e39df3d97552fe412a370eed68abfbfd0542bc
taryncodes@github.com <taryn.bilsborough@gmail.com>
2024-01-12 13:17:26 +0000

    While loops, functions and reeborgs world challenges

---
+++ PycharmProjects/Python-course/Python-course-work/Day_6/dayfive.py
@@ -0,0 +1,15 @@
+# Functions
+# Python has so many built in functions like print, range, len
+num_char = len("hello")
+print(num_char)
+
+def my_function():
+    print("hello")
+    print("bye")
+
+my_function() # calling the function
+
+# while loops
+
+# Completed the hurdle exercises on Reeborgs world
+# Completed the map challenge on Reeborgs world
